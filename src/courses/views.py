1553f6b29dc429d84f600fdd679465b00534adb7
oxfos <rinaldo_bertossa@hotmail.com>
2020-09-27 10:34:01 +0200

    Refactored by adding mixins for view classes.

--- src/courses/views.py
+++ src/courses/views.py
@@ -1,13 +1,55 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Course
 
 
-class ManageCourseListView(ListView):
-    """View to see list of courses of the user."""
-    model = Course
-    template_name = 'courses/manage/course/list.html'
 
+### MIXINS
+
+class OwnerMixin(object):
+    """Mixin to filter queryset by the owner."""
     def get_queryset(self):
         qs = super().get_queryset()
-        return qs.filter(owner=self.request.user)
+        return qs.filter(owner = self.request.user)
+
+
+class OwnerEditMixin(object):
+    """Mixin to ,,, """
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class OwnerCourseMixin(OwnerMixin):
+    model = Course
+
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+    template_name = 'courses/manage/course/form.html'
+
+
+
+### VIEW CLASSES
+
+
+class ManageCourseListView(OwnerCourseMixin, ListView):
+    """View to see list of courses of the user."""
+    template_name = 'courses/manage/course/list.html'
+
+
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    pass
+
+
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+    pass
+
+
+class CourseDeleteView(OwnerCourseEditMixin, DeleteView):
+    template_name = 'courses/manage/course/delete.html'
+    success_url = reverse_lazy('manage_course_list')
+
